ddd987e4321c7d9ccb641041cc01ac523f96fc74
Karthik Venkatesh <kvenkate@tcd.ie>
2023-03-29 01:00:55 +0100

    remove complete

--- client/index.js
+++ client/index.js
@@ -96,54 +96,39 @@ app.post('/upload', (req, res) => {
       })  
 });    
 
-// app.post('/download', (req, res) => {
-//     //get public key from server
-//     axios.post('http://localhost:1234/publickey', {}
-//     ).then(function (response) {
-//         console.log(response.data);
-//         //decode base64 public key that we received from server
-//         // let buff = new Buffer(response.data, 'base64');
-//         let text = Buffer.from(response.data, 'base64');
-//         let publickey = `-----BEGIN PUBLIC KEY-----\n${text.toString('base64')}\n-----END PUBLIC KEY-----\n`;
 
-//         let downloadFile= req.headers.filename;
-//         let username = req.headers.username;
-    
-//         let encryptedFile = crypto.publicEncrypt({
-//             key: publickey,
-//             padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-//             oaepHash: "sha256",
-//         },
-//             Buffer.from(downloadFile)
-//         );
-
-//         const rsaPublicKey = fs.readFileSync(path.resolve(__dirname, './keys/rsa_public_key.pem'));
-//         const encodedRsaPublickey = Buffer.from(rsaPublicKey).toString('base64');
-
-//         // axios.post('http://localhost:1234/publickey', {
-//         //     publicKey:encodedRsaPublickey,
-//         //     filename: encryptedFile,
-//         //     auth: encryptedUsername
-//         // }).then(function (response) {
-            
-
-//         // }).catch(function (error) {
-//         //     console.log(error);
-//         // });
-
-
-//     }).catch(function (error) {
-        
-//         console.log(error);
-//     });
-
-// });
 
 app.post('/remove', async (req, res) => {
     let response = await axios.get('http://localhost:1234/publickey', {});
     let serverPublicKey = response.data;
-    console.log(req.body.file);
+
+    let removeFile = req.body.file;
+
+    encryptedFileName = crypto.publicEncrypt({
+        key: serverPublicKey,
+        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+        oaepHash: "sha256",
+    },
+        Buffer.from(removeFile)
+    );
+
+    let buff = Buffer.from(encryptedFileName);
+    let base64FileName = buff.toString('base64');
+
+    axios.post('http://localhost:1234/remove', {
+            "fileName": base64FileName,
+        }).then(function (response) {  
+            console.log(response.data);
+            if(response.statusCode === 200){
+                res.statusCode = 200;
+            }else{
+                res.statusCode = 400;
+            }
+        }).catch(function (error) {
+            console.log(error);
+        });
 })
+
 
 app.listen(4321, () => {
     console.log('Client listening on port 4321!');
--- server/index.js
+++ server/index.js
@@ -56,6 +56,46 @@ app.post('/upload', (req, res) => {
 });
 
 
+app.get('/upload', (req, res) => {
+
+});
+
+
+app.post('/remove', (req, res) => {
+  let removeFile = req.body.fileName;
+  removeFile = removeFile.toString('ascii');
+  // let buffer = Buffer.from(removeFile);
+  // let newRemoveFile = buffer.toString('ascii');
+  // console.log('Decrypted file: ', newRemoveFile);
+  
+ 
+  let rsaPrivKeyServer = fs.readFileSync(path.resolve(__dirname, './access/rsaprivateServer.pem'), 'utf8');
+
+  const decryptedData = crypto.privateDecrypt(
+    {
+      key: rsaPrivKeyServer,
+      // In order to decrypt the data, we need to specify the
+      // same hashing function and padding scheme that we used to
+      // encrypt the data in the previous step
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: "sha256",
+    },
+    Buffer.from(removeFile, "base64")
+  );
+ 
+  fs.unlink(__dirname + '/uploadedFiles/' + decryptedData, (err) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log('File is removed successfully.');
+    return res.status(200).send('File is removed successfully.');
+  })
+
+
+});
+
+
 //return server's public key
 app.get('/publickey', (req, res) => {
     let rsapublicServer = fs.readFileSync(path.resolve(__dirname, './access/rsapublicServer.pem'), 'utf8');
@@ -75,25 +115,25 @@ app.get('/fileList', (req, res) => {
 app.listen(1234, () => {
     console.log('Server listening on port 1234!');
 
-    //uncomment the following code if you want to generate a new set of AES and RSA pub/priv keys
+    // uncomment the following code if you want to generate a new set of AES and RSA pub/priv keys
 
-    // const {publicKey, privateKey} = crypto.generateKeyPairSync('rsa', {
-    //     modulusLength: 2048,
-    // });
+    const {publicKey, privateKey} = crypto.generateKeyPairSync('rsa', {
+        modulusLength: 2048,
+    });
 
-    // const exportedPublicKeyBuffer = publicKey.export({
-    //     type: "pkcs1",
-    //     format: "pem",
-    //   });
-    //   fs.writeFileSync("access/rsapublicServer.pem", exportedPublicKeyBuffer, { encoding: "utf-8" });
+    const exportedPublicKeyBuffer = publicKey.export({
+        type: "pkcs1",
+        format: "pem",
+      });
+      fs.writeFileSync("access/rsapublicServer.pem", exportedPublicKeyBuffer, { encoding: "utf-8" });
 
-    // const exportedPrivateKeyBuffer = privateKey.export({
-    //   type: "pkcs1",
-    //   format: "pem",
-    // });
-    // fs.writeFileSync("access/rsaprivateServer.pem", exportedPrivateKeyBuffer, {
-    //   encoding: "utf-8",
-    // });
+    const exportedPrivateKeyBuffer = privateKey.export({
+      type: "pkcs1",
+      format: "pem",
+    });
+    fs.writeFileSync("access/rsaprivateServer.pem", exportedPrivateKeyBuffer, {
+      encoding: "utf-8",
+    });
 
     // let aesKey = crypto.randomBytes(32).toString('hex');
     // fs.writeFileSync("access/aeskeyServer.pem", aesKey, {
